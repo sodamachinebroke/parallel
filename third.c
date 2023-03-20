b4a9e8320412ea17b8c5e68a41d5bd3fef3f9720
sodamachinebroke <erseknorbi2000@gmail.com>
2023-03-20 13:16:48 +0100

    generating random doubles and integers. Gonna finish at home i guess.

--- third.c
+++ third.c
@@ -1,5 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int randGenInt(int min, int max)
+{
+    return (rand() % (max - min + 1)) + min; 
+}
+double randGenDouble(double min, double max) 
+{
+    double range = (max - min); 
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
+
+binarySearch(double * array, )
+
+
 int main()
 {
-    printf("hello\n");
+    srand(time(NULL));
+    int workArray[5];
+    workArray[0]= randGenInt(0,1000000);
+    printf("%d\n", workArray[0]);
+    for (int i = 1; i < 5; i++)
+    {
+        workArray[i] = randGenInt(workArray[i-1],1000000);
+        printf("%d\n", workArray[i]);
+    }
+    double workArray2[50];
+    workArray2[0] = randGenDouble(0,1000000);
+    printf("%lf\n", workArray2[0]);
+    for (int i = 1; i < 10; i++)
+    {
+        workArray2[i] = randGenDouble(workArray2[i-1], 1000000);
+        printf("%lf\n", workArray2[i]);
+    }
+    
+    
     return 0;
 }
