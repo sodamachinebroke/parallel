b3324a938b2c43e11e554e676ed335ca4852d394
sodamachinebroke <erseknorbi2000@gmail.com>
2023-03-28 10:20:13 +0200

    Commented everything out.

--- third.c
+++ third.c
@@ -3,14 +3,14 @@
 #include <time.h>
 #include <stdbool.h>
 
-double randGenDouble(double min, double max)
+double randGenDouble(double min, double max) // need this to generate a random double
 {
     double range = (max - min);
     double div = RAND_MAX / (range / 2);
     return min + (rand() / div);
 }
 
-int iterativeBinarySearch(double *array, double n, int low, int high)
+int iterativeBinarySearch(double *array, double n, int low, int high) // self-explanatory
 {
     while (low != high)
     {
@@ -24,7 +24,7 @@ int iterativeBinarySearch(double *array, double n, int low, int high)
     }
 }
 
-int recursiveBinarySearch(double *array, double n, int low, int high)
+int recursiveBinarySearch(double *array, double n, int low, int high) // self-explanatory pt2
 {
     if (low > high)
         return 9999999;
@@ -39,12 +39,12 @@ int recursiveBinarySearch(double *array, double n, int low, int high)
             return recursiveBinarySearch(array, n, low, mid - 1);
     }
 }
-int randInt(int high)
+int randInt(int high) // this one makes a random integer
 {
     return (rand() % (high + 1));
 }
 
-void iterBinSearchArrayTest(int arrlen, double limit)
+void iterBinSearchArrayTest(int arrlen, double limit) // generates and searches an array with iterative binary search
 {
     printf("\nIterative Binary Search Test in a randomly generated floating point array with %d numbers.", arrlen);
     srand(time(NULL));                       // randomizing
@@ -65,22 +65,22 @@ void iterBinSearchArrayTest(int arrlen, double limit)
     printf("\nTime spent with the iterative method: %lf", timeSpentIter);
 }
 
-void recBinSearchArrayTest(int arrlen, double limit)
+void recBinSearchArrayTest(int arrlen, double limit) // this one does the same as the previous but with a recursive binary search
 {
     printf("\nRecursive Binary Search Test in a randomly generated floating point array with %d numbers.", arrlen);
     srand(time(NULL));
-    double workArray2[arrlen];
-    workArray2[0] = randGenDouble(0, limit);
+    double workArray2[arrlen];               // generating array that is arrlen sized
+    workArray2[0] = randGenDouble(0, limit); // this one generates a random double to fill the first value in the double array
     for (int i = 1; i < arrlen; i++)
-        workArray2[i] = randGenDouble(workArray2[i - 1], limit);
+        workArray2[i] = randGenDouble(workArray2[i - 1], limit); // filling the array with randoms; this also makes it so it's strictly monotincally increasing
     srand(time(NULL));
-    double randomNumber = workArray2[randInt(arrlen - 1)];
+    double randomNumber = workArray2[randInt(arrlen - 1)]; // let's find a random index in the array, that we're gonna make the algorithm look for
     printf("\nThe one we are looking for: %lf", randomNumber);
     printf("\nRecursive binary search starting");
-    clock_t beginRec = clock();
-    printf("\nThe number you were looking for is at %d position", recursiveBinarySearch(workArray2, randomNumber, 0, arrlen));
-    clock_t endRec = clock();
-    double timeSpentRec = (double)(endRec - beginRec) / CLOCKS_PER_SEC;
+    clock_t beginRec = clock();                                                                                                // performance monitoring
+    printf("\nThe number you were looking for is at %d position", recursiveBinarySearch(workArray2, randomNumber, 0, arrlen)); // magic
+    clock_t endRec = clock();                                                                                                  // end of monitoring
+    double timeSpentRec = (double)(endRec - beginRec) / CLOCKS_PER_SEC;                                                        // calculating the time spent with magic
     printf("\nTime spent with the recursive method: %lf", timeSpentRec);
 }
 int main()
