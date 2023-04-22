128c581a602ab600c4b2b16f0d3b42c63e81cc44
sodamachinebroke <erseknorbi2000@gmail.com>
2023-04-22 18:56:35 +0200

    Made it work with POSIX threads.

--- temp/temp1.c
+++ temp/temp1.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #define N 20
-#define RECURSIVE_DEPTH 4
+#define NUM_THREADS 4
 
-double recursive_product(double a[], int n, int depth) {
-  if (n == 0) {
-    return 1.0;
-  } else if (n == 1 || depth == 0) {
-    return a[0];
-  } else {
-    int mid = n / 2;
-    return recursive_product(a, mid, depth - 1) * recursive_product(&a[mid], n - mid, depth - 1);
+double a[N];
+double product = 1.0;
+
+void *calculate_product(void *arg) {
+  int tid = *(int *)arg;
+  int start = tid * N / NUM_THREADS;
+  int end = (tid + 1) * N / NUM_THREADS;
+
+  for (int i = start; i < end; i++) {
+    product *= a[i];
   }
+
+  pthread_exit(NULL);
 }
 
 int main() {
-  int i;
-  double a[N];
-  
+  int i, rc;
+  pthread_t threads[NUM_THREADS];
+  int thread_args[NUM_THREADS];
+
   // Fill array with random numbers between 0.1 and 10.0
   srand(12345);
   for (i = 0; i < N; i++) {
@@ -26,8 +32,24 @@ int main() {
     printf("%lf\n", a[i]);
   }
 
-  // Calculate product recursively
-  double product = recursive_product(a, N, RECURSIVE_DEPTH);
+  // Create threads and calculate product
+  for (i = 0; i < NUM_THREADS; i++) {
+    thread_args[i] = i;
+    rc = pthread_create(&threads[i], NULL, calculate_product, &thread_args[i]);
+    if (rc) {
+      printf("ERROR: return code from pthread_create() is %d\n", rc);
+      exit(-1);
+    }
+  }
+
+  // Wait for threads to finish and collect results
+  for (i = 0; i < NUM_THREADS; i++) {
+    rc = pthread_join(threads[i], NULL);
+    if (rc) {
+      printf("ERROR: return code from pthread_join() is %d\n", rc);
+      exit(-1);
+    }
+  }
 
   printf("The product of the elements in the array is %lf\n", product);
   
